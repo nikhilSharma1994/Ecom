b33f617538207bedcbe69bf2653032cfe190f12e
nikhilSharma1994 <me.nikhil.sharma94@gmail.com>
2025-10-16 15:17:20 +0530

    add-backend

---
+++ back-end/config/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+
+const connectDB = async () => {
+    mongoose.connection.on('connected',() => {
+        console.log("DB connected ")
+    })
+   
+
+
+    await mongoose.connect(`${process.env.MONGODB_URI}`)
+
+}
+
+export default  connectDB;
